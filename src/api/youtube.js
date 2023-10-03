b80f5abe2c805a9ac2e7f754b433eb0ed6b1d95c
Gulam Muhammad Moosa Kazim <moosakazim16@gmail.com>
2023-10-03 19:31:21 -0400

    updated with API Key

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const Key = "your api key";
+const Key = process.env.REACT_APP_API_KEY;
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
